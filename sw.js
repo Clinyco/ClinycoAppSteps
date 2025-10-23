8bca6bd22998d1d04b226fd789a7f29970d983fb
villagran-gif <villagran@clinyco.cl>
2025-10-23 09:55:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("clynico-v1").then(cache => {
+      return cache.addAll(["/", "/index.html", "/favicon.ico"]);
+    })
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
