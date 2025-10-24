638f376fb9c2986c15dae542b29be7899cab0d4c
villagran-gif <villagran@clinyco.cl>
2025-10-24 07:31:36 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,16 @@
-const CACHE = "clynico-v4";
-const FILES = ["/", "/index.html", "/manifest.json", "/favicon.ico", "/icons/icon-192.png", "/icons/icon-512.png"];
-
+const CACHE = "clynico-v6"; // <-- súbelo
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
+  self.skipWaiting(); // toma control al instalar
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["/","/index.html","/manifest.json"])));
 });
-self.addEventListener("activate", e => {
+self.addEventListener("activate", e=>{
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
   );
   self.clients.claim();
 });
-self.addEventListener("fetch", e => {
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(resp => resp || fetch(e.request).then(r => {
-        const copy = r.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return r;
-      }))
-    );
-  }
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
